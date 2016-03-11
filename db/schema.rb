a354877be45fd502fa424ac89b65fab53eb4a759
kaiservelilia <veliliakaizer12345@gmail.com>
2016-03-11 10:47:54 +0800

    company profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310070134) do
+ActiveRecord::Schema.define(version: 20160311024620) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "company_name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160310070134) do
     t.string   "address"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "item_categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
