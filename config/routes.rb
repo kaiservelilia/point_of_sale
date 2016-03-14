59c5da4cb0efddf476d7fd9dbb9d72078cae02cd
kaiservelilia <veliliakaizer12345@gmail.com>
2016-03-14 12:59:27 +0800

    companyedit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
 root to: "pages#index"
-resources :companies
+
 
 namespace :management do
 		resources :item_categories
 		resources :item_types
 		resources :employees
+		resources :companies
 end
 
 end
